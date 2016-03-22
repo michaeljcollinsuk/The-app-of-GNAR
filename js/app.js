ab6c6de2f1282101166cd4afe40121f6641f9f4f
Apple <sarah.crawley6@gmail.com>
2016-03-22 13:27:23 +0000

    map function working

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-var gnarApp = angular.module('GnarApp', ['ngResource']);
+var gnarApp = angular.module('GnarApp', ['ngResource', 'uiGmapgoogle-maps'])
+
+
+.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyCEB4CX4SEb3NzorvQZQ18SjB3JpGO8mpw',
+        v: '3.22',
+        libraries: 'weather,geometry,visualization'
+    });
+})
+
+.controller("mapController", function($scope, uiGmapGoogleMapApi) {
+    uiGmapGoogleMapApi.then(function(maps) {
+      $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    });
+});
