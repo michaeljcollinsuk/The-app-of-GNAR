30369ae59aeecbb159a94d1e791a56903dc147ba
Apple <sarah.crawley6@gmail.com>
2016-03-23 18:48:26 +0000

    weather api factory set up and working

--- js/controllers/locationController.js
+++ js/controllers/locationController.js
@@ -1,27 +1,19 @@
-gnarApp.controller('locationController', ['ApiFactory', '$routeParams', 'apiService', '$http' ,function(ApiFactory, $routeParams, apiService, $http) {
+gnarApp.controller('locationController', ['ApiFactory', 'WeatherApiFactory', '$routeParams', 'apiService', '$http' ,function(ApiFactory, WeatherApiFactory, $routeParams, apiService, $http) {
   var self = this;
-  var long;
-  var lat;
-  var key;
-  var url;
-
+  var weatherApiFactory = new WeatherApiFactory();
 
   apiService.getBeaches().then(function(response){
+
     self.chosenLocation = response[($routeParams.id)-1];
-    long = self.chosenLocation.longitude;
-    lat = self.chosenLocation.latitude;
-    key = '&appid=2c2eacccda9dcbe936dee48b6353bab0';
-    url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&units=metric' + key;
-    $http.get(url)
-     .then(function(response){
-       self.weather = response.data;
-     });
+
+    var long = self.chosenLocation.longitude;
+    var lat = self.chosenLocation.latitude;
+
+    weatherApiFactory.getWeather(long, lat).then(function(response){
+      self.weather = response;
+    });
+
    });
-
-
-
-
-
 
 
 
---
+++ js/services/weatherApiService.js
@@ -0,0 +1,26 @@
+gnarApp.service('weatherApiService',['$http', function($http) {
+  var self = this;
+
+  self.getWeather = function(long, lat) {
+    var key = '&appid=2c2eacccda9dcbe936dee48b6353bab0';
+    var url = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&units=metric' + key;
+    return $http.get(url)
+    .then(function(response){
+      return response.data;
+    });
+  };
+
+}]);
+
+
+// apiService.getBeaches().then(function(response){
+//
+//   self.chosenLocation = response[($routeParams.id)-1];
+//
+//   var long = self.chosenLocation.longitude;
+//   var lat = self.chosenLocation.latitude;
+//   weatherApiService.getWeather(long, lat).then(function(response) {
+//     self.weather = response;
+//   });
+//
+//  });
