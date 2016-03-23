1a683689d29f149ca3d2c6dc64a757657e379364
Henry Gordon <hwgordon247@gmail.com>
2016-03-23 18:43:31 +0000

    markers with links... woohoo

--- js/controllers/mapController.js
+++ js/controllers/mapController.js
@@ -2,22 +2,31 @@ gnarApp.controller("mapController", ['uiGmapGoogleMapApi', '$geolocation', 'MapF
 
   apiService.getBeaches().then(function(response){
     self.beachLocations = response;
-
+    // self.coords = {
+    //   latitude: self.beachLocations[0].latitude,
+    //   longitude: self.beachLocations[0].longitude
+    // }
     self.ids = [];
+
 
     for(var i=0; i<self.beachLocations.length; i++){
       self.ids.push(
         {id: self.beachLocations[i].id,
-          coords: {latitude: self.beachLocations[i].latitude,
-            longitude: self.beachLocations[i].longitude}}
-          )
-    }
+        name: self.beachLocations[i].name,
+        coords: {latitude: self.beachLocations[i].latitude, longitude: self.beachLocations[i].longitude}
+        }
+      )};
 
    });
 
   var self = this;
 
   self.factory = new MapFactory();
+
+  self.onClick = function(marker, eventName, model) {
+       self.show = !self.show;
+
+   };
 
 
 
