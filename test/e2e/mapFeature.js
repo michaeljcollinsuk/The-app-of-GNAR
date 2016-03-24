bbfbc62bf3c8b8af54b81eab4426ec7138415db7
Heather <campbell.hf@gmail.com>
2016-03-24 18:19:50 +0000

    Test configured to find a singular marker on a page.

---
+++ test/e2e/mapFeature.js
@@ -0,0 +1,21 @@
+describe('Map page', function() {
+
+
+
+  it('plots markers', function() {
+    browser.get('http://localhost:8080');
+    setTimeout(function(){
+      element(by.id('marker')).click();
+      expect(browser.getTitle()).toEqual('The App of GNAR');
+    }, 10000);
+  });
+
+  it('display weather info when marker clicked', function() {
+
+  });
+
+  it('routes to correct location when more info clicked', function() {
+
+  });
+
+});
