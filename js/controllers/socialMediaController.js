c8e06fa34f2589c03622ac08f14445f5bbfdc549
Apple <sarah.crawley6@gmail.com>
2016-03-26 18:41:43 +0000

    social media controller refactored

--- js/controllers/socialMediaController.js
+++ js/controllers/socialMediaController.js
@@ -1,14 +1,8 @@
 gnarApp.controller('socialMediaController', ['chosenLocationService', 'instagramService', function(chosenLocationService, instagramService) {
   var self = this;
-  self.showInstaFeed = false;
   self.currentLocation = chosenLocationService.selectedLocation;
 
   self.tag = instagramService.generateTag(self.currentLocation.name);
   instagramService.loadInstagram(self.tag);
-
-  self.displayInstaFeed = function() {
-    self.showInstaFeed = true;
-  };
-
 
 }]);
