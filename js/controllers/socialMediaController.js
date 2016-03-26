085927139beca83ef8766a04c7e12a45a05671d0
Michael Collins <michaelj.collinsuk@gmail.com>
2016-03-26 18:25:04 +0000

    Created instagram template

--- js/controllers/socialMediaController.js
+++ js/controllers/socialMediaController.js
@@ -1,8 +1,14 @@
-gnarApp.controller('socialMediaController', ['chosenLocationService', 'instagramService', '$scope', function(chosenLocationService, instagramService, $scope) {
+gnarApp.controller('socialMediaController', ['chosenLocationService', 'instagramService', function(chosenLocationService, instagramService) {
+  var self = this;
+  self.showInstaFeed = false;
   self.currentLocation = chosenLocationService.selectedLocation;
 
   self.tag = instagramService.generateTag(self.currentLocation.name);
   instagramService.loadInstagram(self.tag);
+
+  self.displayInstaFeed = function() {
+    self.showInstaFeed = true;
+  };
 
 
 }]);
