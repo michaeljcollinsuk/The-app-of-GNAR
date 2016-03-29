ad2d6dfa893cad86ce174eb5f6ee97fb9011a424
Henry Gordon <hwgordon247@gmail.com>
2016-03-29 18:04:28 +0100

    working for one beach but database needs sorting

--- js/controllers/webcamController.js
+++ js/controllers/webcamController.js
@@ -2,8 +2,13 @@ gnarApp.controller('webcamController',
 ['chosenLocationService',
 function(chosenLocationService) {
   var self = this;
-  self.webcam = chosenLocationService.selectedLocation.webcam;
-  
 
+  self.webcam = chosenLocationService.selectedLocation.webcam;
+
+  if (self.webcam === null) {
+    self.show = true;
+  } else {
+    self.show = false;
+  };
 
 }]);
