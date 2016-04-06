99695af72ba041d0522c5a88970cdb88ad08f7fe
Apple <sarah.crawley6@gmail.com>
2016-04-06 12:31:04 +0100

    http removed from chosen location factory api call

--- js/factories/chosenLocationFactory.js
+++ js/factories/chosenLocationFactory.js
@@ -5,7 +5,7 @@ gnarApp.factory('chosenLocationFactory',['$http', 'filterWeatherFactory', functi
 
   self.getMarineInfo = function(lat, long) {
     var key = 'key=13b7cf31eb9c40758b7144945162403';
-    var url = 'http://api.worldweatheronline.com/premium/v1/marine.ashx?' + key + '&q=' + lat + ',' + long + '&tide=yes&format=json';
+    var url = '//api.worldweatheronline.com/premium/v1/marine.ashx?' + key + '&q=' + lat + ',' + long + '&tide=yes&format=json';
     return $http.get(url)
     .then(function(response) {
       var data = filterWeatherFactory.sortData(response.data.data.weather);
@@ -15,7 +15,7 @@ gnarApp.factory('chosenLocationFactory',['$http', 'filterWeatherFactory', functi
 
   self.getForecast = function() {
     var key = 'key=13b7cf31eb9c40758b7144945162403';
-    var url = 'http://api.worldweatheronline.com/premium/v1/marine.ashx?' + key + '&q=' + self.location.latitude + ',' + self.location.longitude + '&tide=yes&format=json';
+    var url = '//api.worldweatheronline.com/premium/v1/marine.ashx?' + key + '&q=' + self.location.latitude + ',' + self.location.longitude + '&tide=yes&format=json';
     return $http.get(url);
   };
 
